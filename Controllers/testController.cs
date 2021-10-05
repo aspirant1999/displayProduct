a9bb641ad1985f6586b35e21a63e42a0445e7068
aspirant1999 <adarshpakkala1234@gmail.com>
2021-10-05 19:06:04 +0530

    Add project files.

---
+++ Controllers/testController.cs
@@ -0,0 +1,24 @@
+﻿using displayProduct.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace displayProduct.Controllers
+{
+   
+    public class testController : Controller
+    {
+        
+        // GET: test
+        public ActionResult Index()
+        {
+            testEntities DB = new testEntities();
+
+            List<Product> products = DB.Product.ToList();
+
+            return View(products);
+        }
+    }
+}
